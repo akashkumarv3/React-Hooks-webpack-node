8a7323f5e83a7adb2e7b56ca5a6eca3fddd0b86c
akashkumarv3 <87967921+akashkumarv3@users.noreply.github.com>
2024-02-27 16:35:49 +0530

    added menu and css

--- src/Menu.js
+++ src/Menu.js
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
 
 const Menu=()=>{
-  return <div className="menu">
-          <ul>
-            <li>Home</li>
-            <li>Contact</li>
-            <li>Service</li>
-          </ul>
+  const [menuItem,setMenuItems]=useState('')
+  const menuItems=[
+    {
+      text:"Home",
+      path:"/home"
+    },
+    {
+      text:"About",
+      path:"/about"
+    },
+    {
+      text:"Contact",
+      path:"/contact"
+    }
+  ]
 
+  const fnMenuClick=(text)=>{
+    setMenuItems(text)
+  }
+  
+  //return dont disturb beow
+  return <div >
+    <BrowserRouter>
+    <div>
+          <ul className="menu">
+            {
+              menuItems.map((obj,index)=>{
+                 return <li className={`${menuItem ==obj.text && 'menu-active'}`} onClick={()=>fnMenuClick(obj.text)} key={index}><Link to={obj.path}>{obj.text}</Link></li>
+              })
+            }
+            
+            
+          </ul>
+          </div>
+          <Routes>
+        <Route path="/" exact Component={Home}/>
+        <Route path="/home" Component={Home}/>
+        <Route path="/about" Component={About}/>
+        <Route path="/contact" Component={Contact}/>
+         </Routes>
+          </BrowserRouter>
 
   </div> 
 
